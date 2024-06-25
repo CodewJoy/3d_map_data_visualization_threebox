996c0aaa555703f544d68ddf5350978169722211
code_with_joy <yichen.hung6699@gmail.com>
2024-06-25 22:16:14 +0800

    add legend & terrain layer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import "./App.css";
 import Map from "./components/Map";
 
+export const magnitudeConfig = [
+  { max: 2.5, color: "green" },
+  { min: 2.5, max: 4.0, color: "yellow" },
+  { min: 4.0, max: 5.5, color: "orange" },
+  { min: 5.5, max: 7.0, color: "red" },
+  { min: 7.0, color: "darkred" },
+];
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +16,18 @@ function App() {
         Earthquake data visualization with Threebox{" "}
         <a href="https://earthquake.usgs.gov/">Data Source</a>
       </h3>
+      <div className="magnitude">
+        <b>Magnitude</b>
+        {magnitudeConfig.map((el) => (
+          <div className="magnitudeRow">
+            <div
+              className="magnitudeColor"
+              style={{ backgroundColor: el.color }}
+            />
+            {`${el.min || ""} ~ ${el.max || ""}`}
+          </div>
+        ))}
+      </div>
       <main>
         <Map />
       </main>
--- src/components/Map.js
+++ src/components/Map.js
@@ -4,6 +4,7 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import "threebox-plugin/dist/threebox.css";
 import { Threebox } from "threebox-plugin";
 import data from "../data/earthquakeData.json";
+import { magnitudeConfig } from "../App";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiY29kZXdqb3kiLCJhIjoiY2xlenE0dnE2MDFueTNycDJrMnc5Y2twcCJ9.p7bcH7fkoTbnOYDRUyGVsw";
@@ -11,11 +12,15 @@ mapboxgl.accessToken =
 const ThreeDModuleMap = () => {
   const mapContainerRef = useRef(null);
   const getColor = (magnitude) => {
-    if (magnitude <= 2.5) return "green";
-    if (magnitude <= 4.0) return "yellow";
-    if (magnitude <= 5.5) return "orange";
-    if (magnitude <= 7.0) return "red";
-    return "darkred";
+    for (const el of magnitudeConfig) {
+      if (
+        (el.min === undefined || magnitude >= el.min) &&
+        (el.max === undefined || magnitude < el.max)
+      ) {
+        return el.color;
+      }
+    }
+    return "white";
   };
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -27,6 +32,23 @@ const ThreeDModuleMap = () => {
     });
 
     map.on("style.load", function () {
+      // Add the terrain source
+      map.addSource("mapbox-dem", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.terrain-rgb",
+        tileSize: 512,
+        maxzoom: 14,
+      });
+      // Set the terrain
+      map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+
+      // Add a hillshade layer for better visualization
+      map.addLayer({
+        id: "hillshading",
+        source: "mapbox-dem",
+        type: "hillshade",
+      });
+
       map.addLayer({
         id: "custom_layer",
         type: "custom",
@@ -38,31 +60,21 @@ const ThreeDModuleMap = () => {
             enableTooltips: true,
           });
           window.tb = tb;
-          console.log("Data loaded:", data); // Check if data is loaded
           data.features.forEach((point) => {
             const location = point.geometry.coordinates;
+            const depth = -location[2];
             const magnitude = point.properties.mag;
-            console.log(
-              point,
-              location,
-              "mag",
-              point.properties.mag,
-              "height",
-              location[2]
-            );
             const color = getColor(point.properties.mag);
-
             const sphere = tb
               .sphere({
                 color,
                 radius: magnitude * 1000,
                 material: "MeshStandardMaterial",
               })
-              .setCoords([location[0], location[1], location[2]]);
+              .setCoords([location[0], location[1], depth]);
             const time = new Date(point.properties.time).toLocaleString();
-            console.log(time);
             sphere.addTooltip(
-              `${point.properties.title}, Time: ${time}`,
+              `${point.properties.title}; Time: ${time}; Location: ${location[0]}, ${location[1]}, ${location[2]}`,
               true,
               sphere.anchor,
               true,
